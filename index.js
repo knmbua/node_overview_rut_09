9542d18cc8b7e7354ade11fffa390d6df6ded631
Kandyce <knhopkinson23@gmail.com>
2024-10-01 10:30:13 -0400

    finished arrow function basics

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+const mathTools ={
+    num1: 15,
+    num2: 10,
+    sum: function (){
+// return the sum of num1 and num2
+    const nested = ()=> {
+        console.log('nested',this.num1);
+        const moreNested = () =>{
+            console.log ('moreNested', this.num2);
+        }
 
-function printName(userName) {
-    const capitalize = str => {
-        return str. toUpperCase();
+        moreNested();
     }
 
-    const capName = capitalize(userName);
+    nested();
 
-    console.log(capName);
-}
+    return this.num1 + this.num2;
+},
+    difference: function(){
+// return the difference of num1 and num2 (ie. num1 - num2)
+        return this.num1 - this.num2;
+    }
+};
 
-printName('bob');
+const sum = mathTools.sum();
+const diff = mathTools.difference();
+
+console.log('sum', sum);
+console.log('diff', diff);
